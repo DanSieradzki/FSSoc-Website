df7e99e80d07f39c23c68982ed90332cf28d1b8c
Daniel Sieradzki <dsieradzki@hotmail.co.uk>
2016-09-11 23:42:03 +0100

    Mobile navigation menu work
    hbTapSide works as intended - not showing up when not wanted
    only one accordion opens at a time
    footer menu changes/fixes made
    other small stylistic modifications

--- js/main.js
+++ js/main.js
@@ -1,81 +1,51 @@
-var toggleState = true;
+var toggleState0 = true;
+var toggleState1 = true;
 
-function toggleNav() {
-	if (toggleState == true) {
-		document.getElementById("sideNavId").style.marginLeft = "-116px";
-		document.getElementById("toggleSideNavId").style.backgroundColor = "#333";
-		document.getElementById("toggleSideNavId").innerHTML = "&#10095";
+//toggleNav js code - no need to un-comment it unless for Projects or possibly tutorials page
+
+// function toggleNav() {
+// 	if (toggleState0 == true) {
+// 		document.getElementById("sideNavId").style.marginLeft = "-116px";
+// 		document.getElementById("toggleSideNavId").style.backgroundColor = "#333";
+// 		document.getElementById("toggleSideNavId").innerHTML = "&#10095";
+// 	} else {
+// 		document.getElementById("sideNavId").style.marginLeft = "0px";
+// 		document.getElementById("toggleSideNavId").style.backgroundColor = "transparent";
+// 		document.getElementById("toggleSideNavId").innerHTML = "&#10094";
+// 	}
+// 	toggleState0 = !toggleState0;
+// }
+
+function hbToggleNav() {
+	if (toggleState1 == true) {
+		document.getElementById("myhbSidenav").style.width = "80%";
+		document.getElementById("hbTapSide").style.display = "inline-block";
 	} else {
-		document.getElementById("sideNavId").style.marginLeft = "0px";
-		document.getElementById("toggleSideNavId").style.backgroundColor = "transparent";
-		document.getElementById("toggleSideNavId").innerHTML = "&#10094";
+		document.getElementById("myhbSidenav").style.width = "0px";
+		document.getElementById("hbTapSide").style.display = "none";
 	}
-	toggleState = !toggleState;
+	toggleState1 = !toggleState1;
 }
 
-function hbopenNav() {
-    document.getElementById("myhbSidenav").style.width = "80%";
+
+var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        var setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
 }
 
-function hbcloseNav() {
-    document.getElementById("myhbSidenav").style.width = "0";
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
 }
-
-var accordion = document.getElementsByClassName("accordion");
-accordion[0].onclick = function() {
-	document.getElementById("hbHome").classList.toggle("show");
-}
-var accordion = document.getElementsByClassName("accordion");
-accordion[1].onclick = function() {
-	document.getElementById("hbProjects").classList.toggle("show");
-}
-var accordion = document.getElementsByClassName("accordion");
-accordion[2].onclick = function() {
-	document.getElementById("hbCalendar").classList.toggle("show");
-}
-var accordion = document.getElementsByClassName("accordion");
-accordion[3].onclick= function() {
-	document.getElementById("hbTutorials").classList.toggle("show");
-}
-var accordion = document.getElementsByClassName("accordion");
-accordion[4].onclick = function() {
-	document.getElementById("hbIntranet").classList.toggle("show");
-}
-var accordion = document.getElementsByClassName("accordion");
-accordion[5].onclick = function() {
-	document.getElementById("hbContact").classList.toggle("show");
-}
-
-//for (var i = 0; i < 4; i++)	{
-	//var accordion = document.getElementsByClassName("accordion");
-	//accordion[i].onclick = function() {
-	//	document.getElementById("1516").classList.toggle("show");
-	//	console.log(i+1);
-	//}
-//}
-
-var accordion = document.getElementsByClassName("accordion");
-accordion[6].onclick = function() {
-	document.getElementById("1516").classList.toggle("show");
-	
-}
-accordion[7].onclick = function() {
-	document.getElementById("1415").classList.toggle("show");
-	
-}
-accordion[8].onclick = function() {
-	document.getElementById("1314").classList.toggle("show");
-	
-}
-accordion[9].onclick = function() {
-	document.getElementById("1213").classList.toggle("show");
-	//document.getElementById("mySidenav").style.width = "350px";
-	
-}
-accordion[10].onclick = function() {
-	document.getElementById("1112").classList.toggle("show");
-	//document.getElementById("mySidenav").style.width = "350px";
-	
-}
-
-//edit this to a for loop to go through all accordions
