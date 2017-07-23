2652702ef5ad834d4d8f6b0442fa807a83e28f97
Daniel Sieradzki <dsieradzki@hotmail.co.uk>
2017-07-24 00:10:19 +0100

    some more js cleanup

--- indexTest.php
+++ indexTest.php
@@ -12,45 +12,45 @@
       <h2>Welcome to FSSoc</h2>
     </div><!-- 
      --><div class="projectBoxes" id="aboutUsBox" onclick="expandAboutUs()">
-     <a href="javascript:void(0)" class="expandResetX disappearingClass1" id="expandResetAboutUs" onclick="expandReset">x</a>
+     <a href="javascript:void(0)" class="expandResetX disappearingClass1 resetDisappear" id="expandResetAboutUs" onclick="expandReset">x</a>
         <div class="projectImgCaptionT disappearingClass1" id="projectImgCaptionAboutT">
           <img src="../img/crest.png" id="crest">
           <h1>About us</h1>
-          <p class="disappearingClass1">Find out who we are</p>
-          <div class="expandedBoxDescription disappearingClass1" id="aboutUsExpanded">
+          <p class="disappearingClass1 hoverUnderline">Find out who we are</p>
+          <div class="expandedBoxDescription disappearingClass1 resetDisappear" id="aboutUsExpanded">
             <p>As the only undergraduate flight simulation society in the world, we offer the unique opportunity to develop and test complex aircraft designs. Moreover, the design, communication and leadership skills attained will prove extremely valuable in future studies and work in industry - there simply isn't any way to beat hands on experience.</P>
             <a>Learn more</a>
           </div>
         </div>        
       </div><!-- 
        --><div class="projectBoxes" id="itFliesBox" onclick="expandItFlies()">
-       <a href="javascript:void(0)" class="expandResetX disappearingClass1" id="expandResetItFlies" onclick="expandReset">x</a>
+       <a href="javascript:void(0)" class="expandResetX disappearingClass1 resetDisappear" id="expandResetItFlies" onclick="expandReset">x</a>
           <div class="projectImgCaptionT disappearingClass1" id="projectImgCaptionItFliesT">
             <h1>It Flies</h1>
-            <p class="disappearingClass1">See some of the concepts we've developed</p>
-            <div class="expandedBoxDescription disappearingClass1" id="itFliesExpanded">
+            <p class="disappearingClass1 hoverUnderline">See some of the concepts we've developed</p>
+            <div class="expandedBoxDescription disappearingClass1 resetDisappear" id="itFliesExpanded">
               <p>The Flight Simulation Society enters two teams every year to the IT FLIES UK and USA competitions. The competitions aim to give society members the opportunity to develop the key skills of design and presentations, as well as setting the foundation for future involvement within the society.</p>
-             <a>Learn more</a>     
+              <a>Learn more</a>     
             </div>
         </div>
       </div><!-- 
        --><div class="projectBoxes" id="projectPhoenixBox" onclick="expandProjectPhoenix()">
-       <a href="javascript:void(0)" class="expandResetX disappearingClass1" id="expandResetProjectPhoenix" onclick="expandReset">x</a>
+       <a href="javascript:void(0)" class="expandResetX disappearingClass1 resetDisappear" id="expandResetProjectPhoenix" onclick="expandReset">x</a>
           <div class="projectImgCaptionT disappearingClass1" id="projectImgCaptionPhoenixT">
             <h1>Project Phoenix</h1>
-            <p class="disappearingClass1">Delve into our mission to build a simulator</p>
-            <div class="expandedBoxDescription disappearingClass1" id="projectPhoenixExpanded">
-            <p>Open only to the best senior society members, Project Phoenix gives these members the opportunity to build a custom state-of-the-art flight simulator. The simulator will be constructed from a HAWK Mark II facsimile and will incorporate a glass cockpit, heads up display and G-cueing motion. The simulation for the simulator will be student developed.</p>
-            <a>Learn more</a>
+            <p class="disappearingClass1 hoverUnderline">Delve into our mission to build a simulator</p>
+            <div class="expandedBoxDescription disappearingClass1 resetDisappear" id="projectPhoenixExpanded">
+              <p>Open only to the best senior society members, Project Phoenix gives these members the opportunity to build a custom state-of-the-art flight simulator. The simulator will be constructed from a HAWK Mark II facsimile and will incorporate a glass cockpit, heads up display and G-cueing motion. The simulation for the simulator will be student developed.</p>
+              <a>Learn more</a>
           </div>
         </div>        
       </div><!-- 
        --><div class="projectBoxes" id="aiaaBox" onclick="expandAIAA()">
-       <a href="javascript:void(0)" class="expandResetX disappearingClass1" id="expandResetAIAA" onclick="expandReset">x</a>
+       <a href="javascript:void(0)" class="expandResetX disappearingClass1 resetDisappear" id="expandResetAIAA" onclick="expandReset">x</a>
           <div class="projectImgCaptionT disappearingClass1" id="projectImgCaptionAIAAT">
             <h1>AIAA</h1>
-            <p class="disappearingClass1">Explore our latest venture</p>
-            <div class="expandedBoxDescription disappearingClass1" id="AIAAExpanded">
+            <p class="disappearingClass1 hoverUnderline">Explore our latest venture</p>
+            <div class="expandedBoxDescription disappearingClass1 resetDisappear" id="AIAAExpanded">
               <p>AIAA text</p>
               <a>Learn more</a>
             </div>
--- js/indexTest.js
+++ js/indexTest.js
@@ -2,6 +2,7 @@ var disappearing = document.getElementsByClassName('disappearingClass1');
 var projectBoxMinimised = document.getElementsByClassName('projectBoxes');
 var textAligningLeft = document.getElementsByClassName('projectImgCaptionT');
 var marginLefting = document.getElementsByClassName('projectImgCaptionT');
+var resetDisappearing = document.getElementsByClassName('resetDisappear');
 
 function expandBase()   {
     for (i = 0; i < disappearing.length; i++) {
@@ -14,10 +15,9 @@ function expandBase()   {
        textAligningLeft[i].style.textAlign = "left";
     }
     for (i = 0; i < marginLefting.length; i++) {
-       marginLefting[i].style.marginLeft = "20vh";
+       marginLefting[i].style.marginLeft = "35vh";
     }
 }
-
 function expandAboutUs()    {
     expandBase();
     document.getElementById("aboutUsBox").style.width = "91%";
@@ -26,7 +26,6 @@ function expandAboutUs()    {
     document.getElementById("expandResetAboutUs").style.display = "inline-block";
     document.getElementById("aboutUsExpanded").style.display = "inline-block";
 }
-
 function expandItFlies()    {
     expandBase();
     document.getElementById("itFliesBox").style.width = "91%";   
@@ -34,7 +33,6 @@ function expandItFlies()    {
     document.getElementById("expandResetItFlies").style.display = "inline-block";
     document.getElementById("itFliesExpanded").style.display = "inline-block";
 }
-
 function expandProjectPhoenix()    {
     expandBase();
     document.getElementById("projectPhoenixBox").style.width = "91%";
@@ -42,7 +40,6 @@ function expandProjectPhoenix()    {
     document.getElementById("expandResetProjectPhoenix").style.display = "inline-block";
     document.getElementById("projectPhoenixExpanded").style.display = "inline-block";
 }
-
 function expandAIAA()    {
     expandBase();
     document.getElementById("aiaaBox").style.width = "91%";
@@ -50,26 +47,21 @@ function expandAIAA()    {
     document.getElementById("expandResetAIAA").style.display = "inline-block";
     document.getElementById("AIAAExpanded").style.display = "inline-block";
 }
-
 function expandReset()    {
-    document.getElementById("aboutUsBox").style.width = "25%";
-    document.getElementById("itFliesBox").style.width = "25%";
-    document.getElementById("projectPhoenixBox").style.width = "25%";
-    document.getElementById("aiaaBox").style.width = "25%";
+    for (i = 0; i < disappearing.length; i++) {
+       disappearing[i].style.display = "inline-block";
+    }
+    for (i = 0; i < projectBoxMinimised.length; i++) {
+       projectBoxMinimised[i].style.width = "25%";
+    }
+    for (i = 0; i < textAligningLeft.length; i++) {
+       textAligningLeft[i].style.textAlign = "center";
+    }
+    for (i = 0; i < marginLefting.length; i++) {
+       marginLefting[i].style.marginLeft = "0";
+    }
+    for (i = 0; i < resetDisappearing.length; i++) {
+       resetDisappearing[i].style.display = "none";
+    }    
     document.getElementById("crest").style.display = "none";
-
-    document.getElementById("projectImgCaptionAboutT").style.display = "block";
-    document.getElementById("projectImgCaptionItFliesT").style.display = "block";
-    document.getElementById("projectImgCaptionPhoenixT").style.display = "block";
-    document.getElementById("projectImgCaptionAIAAT").style.display = "block";
-
-    document.getElementById("aboutUsExpanded").style.display = "none";
-    document.getElementById("itFliesExpanded").style.display = "none";
-    document.getElementById("projectPhoenixExpanded").style.display = "none";
-    document.getElementById("AIAAExpanded").style.display = "none";
-
-    document.getElementById("expandResetAboutUs").style.display = "none";
-    document.getElementById("expandResetItFlies").style.display = "none";
-    document.getElementById("expandResetProjectPhoenix").style.display = "none";
-    document.getElementById("expandResetAIAA").style.display = "none";
 }
